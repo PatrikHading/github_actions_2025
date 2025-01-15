fa03eab087d3139d80f220850b29a2f58ec182a9
hading <hading@gmail.com>
2025-01-15 20:46:01 +0100

    Updated Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Github Actions!!");
+        System.out.println("Test test test");
     }
 }
